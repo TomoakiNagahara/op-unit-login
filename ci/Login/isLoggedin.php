f93d629e63671051cf4054f217773085590b8b05
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-02 19:06:12 +0900

    New: Login :: isLoggedin() : ?bool

---
+++ ci/Login/isLoggedin.php
@@ -0,0 +1,22 @@
+<?php
+/**	op-unit-login:/ci/Login/isLoggedin.php
+ *
+ * @created     2025-05-05
+ * @version     1.0
+ * @package     op-unit-login
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci UNIT\CI\CI_Config */
+
+//	isLoggedin
+$method = 'isLoggedin';
+$args   =  null;
+$result =  null;
+$ci->Set($method, $result, $args);
