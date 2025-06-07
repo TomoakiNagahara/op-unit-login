58de5d6ff258a3b6fab9926a2602020c87307ab2
TomoakiMac5K 2019i9 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-07 17:15:47 +0900

    New: SignIn :: QQL()

---
+++ ci/SignIn/QQL.php
@@ -0,0 +1,22 @@
+<?php
+/**	op-unit-login:/ci/SignIn/QQL.php
+ *
+ * @created     2025-06-07
+ * @version     1.0
+ * @package     op-unit-login
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci UNIT\CI\CI_Config */
+
+//	QQL
+$method = 'QQL';
+$args   =  null;
+$result = 'OP\UNIT\QQL';
+$ci->Set($method, $result, $args);
