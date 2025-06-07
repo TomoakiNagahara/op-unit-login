339347b6e77901cfc3dbd4612c6cea26d47baa85
TomoakiMac5K 2019i9 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-07 14:19:53 +0900

    New: SignIn :: Auto()

--- SignIn.class.php
+++ SignIn.class.php
@@ -36,4 +36,41 @@ class SignIn implements IF_UNIT
 	 */
 	use OP_CORE, OP_CI;
 	use OP_TEMPLATE;
+
+	/** Automatically
+	 *
+	 * @created    2025-06-07
+	 */
+	static function Auto()
+	{
+		//	...
+		$form = self::Form();
+
+		//	Check if the form validation.
+		if( $form->isValidate() ){
+
+			//	Submit value is valid.
+			$account  = $form->GetValue('account');
+			$password = $form->GetValue('password');
+
+			//	Check if the credentials are valid.
+			if( self::isCredentials($account, $password) ){
+
+				//	Since auth was successful, store the auth flag in the session.
+				\OP\UNIT\Login::Session('isLoggedin', true);
+
+				//	Failed to validate.
+				self::Template('login/success.phtml');
+
+				//	...
+				return;
+			}
+
+			//	Failed to validate.
+			$message = 'Either the account is not registered or the password is incorrect.';
+		}
+
+		//	Failed to validate.
+		self::Template('login/form.phtml', ['form' => $form, 'message' => $message ?? null]);
+	}
 }
