7a1b27e24a4838d094a5a15baf204562b0864bac
TomoakiMac5K 2019i9 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-08 14:12:30 +0900

    New: SignUp :: Form() : OP\IF_FORM

--- SignUp.class.php
+++ SignUp.class.php
@@ -71,4 +71,24 @@ class SignUp implements IF_UNIT
 		//	...
 		self::Template('register/form.phtml', ['form'=>$form]);
 	}
+
+	/** Form
+	 *
+	 * @created    2025-06-07
+	 * @return     \OP\IF_FORM
+	 */
+	static function Form() : \OP\IF_FORM
+	{
+		/* @var $_form \OP\IF_FORM */
+		static $_form = null;
+
+		//	Instantiate the form only once.
+		if( $_form === null ){
+			$_form = OP()->Unit()->Instantiate('Form');
+			$_form -> Config(__DIR__.'/config/register.php');
+		}
+
+		//	Return the IF_FORM.
+		return $_form;
+	}
 }
