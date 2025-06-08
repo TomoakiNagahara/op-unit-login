99415201b9ee83b2c0aff40e0063c91d0902cab6
TomoakiMac5K 2019i9 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-08 14:12:30 +0900

    New: SignUp :: Form() : OP\IF_FORM

---
+++ ci/SignUp/Form.php
@@ -0,0 +1,22 @@
+<?php
+/**	op-unit-login:/ci/Login/SignUp/Form.php
+ *
+ * @created     2025-06-08
+ * @version     1.0
+ * @package     op-unit-login
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci UNIT\CI\CI_Config */
+
+//	Form
+$method = 'Form';
+$args   =  null;
+$result = 'OP\UNIT\Form';
+$ci->Set($method, $result, $args);
