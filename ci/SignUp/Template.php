bbf78a7646b83c26dfe037438aefc85bb62d92ea
TomoakiMac5K 2019i9 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-08 14:08:29 +0900

    Add: SignUp > use > OP_TEMPLATE

---
+++ ci/SignUp/Template.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-unit-login:/ci/SignUp/Template.php
+ *
+ * @created     2025-06-08
+ * @version     1.0
+ * @package     op-unit-login
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci UNIT\CI\CI_Config */
+
+//	Template
+$method = 'Template';
+$arg1   = 'foo';
+$arg2   = 'bar';
+$args   = ['ci.phtml',['arg1'=>$arg1, 'arg2'=>$arg2]];
+$result = "{$arg1}, {$arg2}";
+$ci->Set($method, $result, $args);
