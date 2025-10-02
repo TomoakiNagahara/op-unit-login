69f5607e032633709e1df7a0e8345ac67c516c63
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-02 18:29:40 +0900

    Add: Init.php | PHPDoc

---
+++ Init.php
@@ -0,0 +1,18 @@
+<?php
+/**	op-asset-model:/Init.php
+ *
+ * @created   2025-10-02
+ * @license   Apache-2.0
+ * @package   op-asset-model
+ * @copyright (C) 2025 Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
