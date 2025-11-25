e84b293765e3cc5a0f18d78c7a68bf223ae90b26
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-25 13:20:59 +0900

    New: webpack > js > login.js

---
+++ webpack/js/login.js
@@ -0,0 +1,11 @@
+
+/**	op-unit-login:/webpack/js/login.js
+ *
+ * @created     2025-11-25
+ * @version     1.0
+ * @package     op-unit-login
+ * @author      Tomoaki Nagahara
+ * @copyright   Tomoaki Nagahara All rights reserved.
+ */
+
+console.log('login.js');
